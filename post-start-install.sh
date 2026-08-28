b70218ecb7c466522128f00ab688477dea492d5c
Daniel Sauerbrun <dsauerbrun@gmail.com>
2026-08-28 16:31:15 +0000

    symlink agents.md

--- post-start-install.sh
+++ post-start-install.sh
@@ -3,7 +3,7 @@ set -x
 
 # All of these segments will be symlinked from the home directory to your EFS drive.
 # You can safely customize this list.
-for segment in .claude .claude.json .codex .cursor .cursor-server; do
+for segment in AGENTS.md .claude .claude.json .codex .cursor .cursor-server; do
   # If you don't want to delete what's currently in the home directory, remove this.
   if [ -e "$HOME/$segment" ]; then
     rm -rf "$HOME/$segment"
